99e59b8f469efac41b25922d5080c035efa1e128
ejenaru <irene.diazfossaty@gmail.com>
2020-09-23 12:28:31 +0200

    get_temperature done, gitignore modified

--- get_temperature/get_temperature.py
+++ get_temperature/get_temperature.py
@@ -1,41 +1,55 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 import datetime
 import requests
-# This function has no input and returns a str with the current date
-def getCurrentDate():
+from bs4 import BeautifulSoup
+
+
+def get_current_date():
 	"""Return the current date in format 'Friday, 28/August/2020'"""
+	tod = datetime.datetime.today()
+	today_f = tod.strftime("%A, %d/%B/%Y")
 
-	# Datetime object 'tod' for saving 'today' date
-	tod= datetime.datetime.today()
+	return today_f
 
-	# Transform 'tod' to our favourite format
-	today_f=tod.strftime("%A, %d/%B/%Y")
 
-	return(today_f)
+def get_page(url):
 
-#This function returns the local temperature based on a website for Madrid
-def getPage(url):
-
-	page_resp= requests.get(url)
-	page_cont=page_resp.content
+	page_resp = requests.get(url)
+	page_cont = page_resp.content
 	return page_cont
 
-def getTemp(page): #todo finish this function
+
+def get_temp(page):
+
+	soup = BeautifulSoup(page, "html.parser")
+	# soup is the html of the page but in a beautiful version
+	table = soup.find(class_="borde_izq_dcha_estado_cielo no_wrap")
+	temp = table.find("div", class_="no_wrap").text
+	# soup is the block that has the information that I want
+	return temp
 
 
-	from bs4 import BeautifulSoup 
-	soup = BeautifulSoup(page_cont,"html.parser")
-	#soup is the html of the page but in a beautifull version
+def save_temp(temp, date, filename="temperature.txt"):
+	text = "Today is {0} and the temperature is: {1}\n".format(date, temp)
 
-	return soup
+	file = open(filename, "a+")
+	file.write(text)
+	file.close()
+	file = open(filename, "r")
+	filecontent = file.read()
+	file.close()
+	return filecontent
+
 
 def main():
-	url="https://www.eltiempo.es/madrid.html"
-	print(getCurrentDate())
-	print(getTemp())
+	url = "http://www.aemet.es/es/eltiempo/prediccion/municipios/madrid-id28079"
+	page = get_page(url)
+	temp = get_temp(page)
+	date = get_current_date()
+
+	print(save_temp(temp, date))
+
 
 if __name__ == "__main__":
-    main()
-
-
+	main()
