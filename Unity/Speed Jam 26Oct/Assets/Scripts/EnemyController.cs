1e6f3327dcf57ac3c1521ecf46e78de451f744cf
Irene Díaz Fossaty <ejenaru@users.noreply.github.com>
2020-11-14 13:56:53 +0100

    Add Unity Project
    SpeedJam

---
+++ Unity/Speed Jam 26Oct/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float maxX = 7, minX = -7, maxY = 3, minY = -3;
+    Vector2 destiny;
+    float changeDirRate;
+    float timeNow;
+    float timeLastDir;
+    Transform player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeNow = Time.time;
+
+        if (Vector2.Distance(destiny, transform.position) > 1)
+        {
+            ChooseDestiny();
+            transform.Translate(destiny * Time.deltaTime);
+        }
+
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "scenario")
+        {
+            ChooseDestiny();
+        }
+        if (other.gameObject.tag == "bullet")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void ChooseDestiny() // create new random vector
+    {
+        destiny = new Vector2(Random.Range(minX, maxX),
+            Random.Range(minY, maxY));
+
+    }
+}
---
+++ Unity/Speed Jam 26Oct/Assets/Scripts/Generator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject coin;
+    public float maxX = 7, minX = -7, maxY = 3, minY = -3;
+    public float spawnEnemyRate;
+    public float spawnCoinRate;
+    float timeLastCoin;
+    float timeLastEnemy;
+    float timeNow;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeNow = Time.time;
+
+        timeLastEnemy = SpawnHere(enemy, spawnEnemyRate, timeLastEnemy);
+        timeLastCoin = SpawnRandom(coin, spawnCoinRate, timeLastCoin);
+    }
+    float SpawnHere(GameObject obj, float spawnRate, float timeLastObject) 
+        //Spawns obj, at that rate, needs the time where the last object was created.
+        //returns the last time the object was created.
+    {
+
+        // TODO insert max number of objects to create
+       if (timeNow - timeLastObject > spawnRate)
+        {
+            Instantiate(obj, transform.position, transform.rotation);
+            timeLastObject = Time.time;
+            
+        }
+        return timeLastObject;
+    }
+    float SpawnRandom(GameObject obj, float spawnRate, float timeLastObject)
+    {
+        if (timeNow - timeLastObject > spawnRate)
+        {
+            Instantiate(obj, RandomPosition(maxX,minX,maxY,minY), Quaternion.identity);
+            timeLastObject = Time.time;
+        }
+        return timeLastObject;
+    }
+    public Vector2 RandomPosition(float max_X, float min_X, float max_Y, float min_Y)
+    {
+        float randomX;
+        float randomY;
+        randomX = Random.Range(min_X, max_X);
+        randomY = Random.Range(min_Y, max_Y);
+        Vector2 position;
+        position = new Vector2(randomX, randomY);
+
+
+        return position;
+
+    }
+}
